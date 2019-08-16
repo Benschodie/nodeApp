63bda624e6ef3b573dc4acc76978ce85ece117dd
Benny <benschodie@gmail.com>
2019-08-16 12:26:09 +0200

    adding env variables to project

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const mongodb = require('mongodb')
 
-const connectionString = 'mongodb+srv://fullstackApp:goFullstack01@cluster0-zjzqt.mongodb.net/FullstackNode01?retryWrites=true&w=majority'
-
-mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     module.exports = client.db()
     // app listen hier damit sichergestellt ist, dass eine verbindung zur datanbank besteht.
     
     // app file wird zugegriffen, siehe dazu export in app file ganz unten. 
     const app = require('./app')
-    app.listen(4000)
+    app.listen(process.env.PORT)
 })
